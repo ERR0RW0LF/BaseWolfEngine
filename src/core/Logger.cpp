348bc34e8b2f957bf6e059e36e944a09b6eb9421
ERR0RW0LF <finn.woma@gmail.com>
2024-11-19 17:19:29 +0100

    Remove CMake configuration and logging implementation files

--- src/core/Logger.cpp
+++
@@ -1,6 +0,0 @@
-#include "Logger.h"
-#include <iostream>
-
-void Logger::Log(const std::string& message) {
-    std::cout << message << std::endl;
-}
--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "core/Logger.h"
-
-int main() {
-    Logger::Log("BaseWolfEngine started.");
-    return 0;
-}
